77cef97decb084a3499eaf4d7540c57fca5305e7
Ayush Gupta <33717350+ayushgs@users.noreply.github.com>
2018-06-08 00:12:09 -0700

    Delete download_data.sh
    Removed download_data

--- Prepare_data/download_data.sh
+++
@@ -1,9 +0,0 @@
-source ~/.bashrc
-
-if [ ! -d ../data/DF_img_pose ]; then
-    cd ../data
-    wget homes.esat.kuleuven.be/~liqianma/NIPS17_PG2/data/DF_img_pose.zip
-    unzip DF_img_pose.zip
-    rm -f DF_img_pose.zip
-    cd -
-fi
